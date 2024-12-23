78b2450ddf92836c1ebe346edf6face9e6402cbb
Logical Guess <logicalguess@gmail.com>
2024-12-23 10:51:03 -0500

    fixed table (direction decimal and column order)

--- src/hume/utils/common.py
+++ src/hume/utils/common.py
@@ -85,30 +85,26 @@ def to_table(s, decimals=4):
 
 
 def state_table_to_string(state, decimals=4, symbol='\u2588'):
-    assert (decimals <= 10)
+    assert(decimals <= 10)
     n = int(log2(len(state)))
     round_state = [round(state[k].real, decimals) + 1j * round(state[k].imag, decimals) for k in range(len(state))]
 
-    headers = ['Outcome', 'Binary', 'Amplitude', 'Magnitude', 'Direction', 'Amplitude Bar', 'Probability']
-    offsets = [max(len(headers[0]), floor(log10(len(state)))),  # outcome
-               max(len(headers[1]), n),  # binary
-               max(len(headers[2]), 2 * (decimals + 2) + 6),  # amplitude
-               max(len(headers[3]), (decimals + 2)),  # magnitude
-               max(len(headers[4]), decimals),  # direction
-               max(len(headers[5]), 24),  # amplitude bar
-               max(len(headers[6]), decimals + 2),  # probability
+    headers = ['Outcome', 'Binary', 'Amplitude', 'Direction', 'Magnitude', 'Amplitude Bar', 'Probability']
+    offsets = [max(len(headers[0]), floor(log10(len(state)))),               # outcome
+               max(len(headers[1]), n),                                      # binary
+               max(len(headers[2]), 2*(decimals + 2) + 6),                   # amplitude
+               max(len(headers[4]), decimals),                               # direction
+               max(len(headers[3]), (decimals + 2)),                         # magnitude
+               max(len(headers[5]), 24),                                     # amplitude bar
+               max(len(headers[6]), decimals + 2),                           # probability
                ]
 
     for i in range(len(offsets)):
-        headers[i] = headers[i] + ' ' * (offsets[i] - len(headers[i]))
+        headers[i] = headers[i] + ' '*(offsets[i] - len(headers[i]))
 
     header_str = '  '.join(headers)
 
-    output = '\n'
-    output += header_str
-    output += '\n'
-    output += len(header_str) * '-'
-    output += '\n'
+    output = '\n' + header_str + '\n' + len(header_str) * '-' + '\n'
 
     for k in range(len(round_state)):
         direction = round(atan2(round_state[k].imag, round_state[k].real) * 180 / pi, 2)
@@ -122,15 +118,13 @@ def state_table_to_string(state, decimals=4, symbol='\u2588'):
                               (' + ' if round_state[k].imag >= 0 else ' - ') + 'i' +
                               str(abs(round_state[k].imag)).ljust(decimals + 2, '0')).ljust(offsets[2] + 1, ' '),
 
-                             str(round(abs(state[k]), decimals)).ljust(decimals + 2, ' ').ljust(offsets[3], ' '),
-
                              (str(((' ' if direction >= 0 else '-') + str(floor(abs(direction)))).rjust(4, ' ') +
-                                  '.' + str(int(100 * round(direction - floor(direction), 2))).ljust(2,
-                                                                                                     '0')) + '\u00b0' if
+                                  '.' + str(int(100*round(abs(direction) - floor(abs(direction)), 2))).ljust(2, '0')) + '\u00b0' if
                               abs(round_state[k]) > 0 else offsets[4] * ' ').ljust(offsets[4], ' '),
 
-                             fg(*[int(255 * a) for a in complex_to_rgb(state[k])]) + (
-                                         int(abs(state[k] * 24)) * symbol).ljust(offsets[5], ' ') + fg.rs,
+                             str(round(abs(state[k]), decimals)).ljust(decimals + 2, ' ').ljust(offsets[3], ' '),
+
+                             fg(*[int(255*a) for a in complex_to_rgb(state[k])]) + (int(abs(state[k] * 24)) * symbol).ljust(offsets[5], ' ') + fg.rs,
 
                              str(round(abs(state[k]) ** 2, decimals)).ljust(decimals + 2, ' ')
                              ])
